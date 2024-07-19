cbf254661cc943d07f3602f97d32e31615062ba8
TheParadox20 <onyambusamson@gmail.com>
2024-07-19 11:58:19 +0300

    revenue stats

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Logs;
+use App\Models\Mpesa;
 
 //populates dashboard with stats
 class StatsController extends Controller
@@ -14,9 +16,13 @@ class StatsController extends Controller
         ->count();
         $certificates = Logs::whereAny(['action'],'LIKE' , '%New certificate request%')
         ->count();
+        $revenue = Mpesa::where('ResultCode', 0)
+                    ->select(DB::raw('SUM(amount) as total_revenue'))
+                    ->get()
+                    ->first();
         return response()->json([
             'revenue' => [
-                'quantity' => number_format(rand(1000, 100000), 0, '.', ','),
+                'quantity' => $revenue->total_revenue,
                 'trend' => (bool)rand(0, 1),
                 'rate' => rand(1, 100)
             ],
