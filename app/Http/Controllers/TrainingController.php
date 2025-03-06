9455d7446bea22dc9222163813b8fd09351bb3f9
TheParadox20 <onyambusamson@gmail.com>
2025-03-06 11:52:28 +0300

    update emails

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -246,11 +246,11 @@ class TrainingController extends Controller
             $name = $certificate->Name;
             $number = $certificate->Number;
             $qrData = 'https://portal.eik.co.ke/verify?training='.$certificate->Training.'&id='.$number;
-            $background = public_path($training->Background);
+            $background = $request->training==2?$background = public_path('/system/custom.jpg'):public_path($training->Background);
             $info = $training->Info;
             $StartDate = new DateTime($training->StartDate);
             $date = 'Date '. $StartDate->format('jS F Y');
-            $pdf = Pdf::loadView('certificates.training', compact(['background', 'name', 'number','qrData', 'info', 'date']));
+            $pdf = Pdf::loadView($request->training==2?'certificates.custom':'certificates.training', compact(['background', 'name', 'number','qrData', 'info', 'date']));
             $pdf->render();
             $pdfContent = $pdf->output();
             EmailController::sendCertificate($certificate->Email, $pdfContent, compact(['name','number']));
